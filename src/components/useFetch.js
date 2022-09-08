b1d7f6290bffdd1267ecce597789dda425d8e630
CarlosCubNed <kuva5008@gmail.com>
2022-09-08 17:12:23 +0200

    abort control to stop fetching

--- src/components/useFetch.js
+++ src/components/useFetch.js
@@ -5,6 +5,7 @@ const useFetch = (url) => {
     const [isPending, setIsPending] = useState(true);
     const [error, setError] = useState(null);
 
+    const abortCont = new AbortController();//This is to stop the fetching once is done and prevent errors
     /*
 This is condiction is to catch an error when do sucessfully fetching but there is an error at the end point of the REST API
                if (!res.ok) {
@@ -12,9 +13,9 @@ This is condiction is to catch an error when do sucessfully fetching but there i
                 }
 */
     useEffect(() => {
-        fetch(url)
+        fetch(url, { signal: abortCont.signal })// { signal: abortCont.signal } necessary for abort control
             .then(res => {
-                if (!res.ok) {
+                if (!res.ok) {// error coming back from server
                     throw Error('could not fetching');
                 }
                 return res.json();
@@ -25,9 +26,13 @@ This is condiction is to catch an error when do sucessfully fetching but there i
                 setError(null);
             })
             .catch(err => {
+                // auto catches network / connection error
                 setIsPending(false);
                 setError(err.message);
             });
+
+        // abort the fetch
+        return () => abortCont.abort();
     }, [url]);
     return { data, isPending, error };
 }
